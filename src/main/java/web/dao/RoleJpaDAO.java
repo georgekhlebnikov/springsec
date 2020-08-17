661ecb58eed8b4e922b41a455185f842c581ad8b
georgekhlebnikov <george.khlebnikov@yandex.ru>
2020-08-17 13:35:08 +0300

    [2.4.2] v2.0

--- src/main/java/web/dao/RoleJpaDAO.java
+++
@@ -1,7 +0,0 @@
-package web.dao;
-
-import web.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleJpaDAO extends JpaRepository<Role, Integer> {
-}
--- src/main/java/web/security/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package web.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-//пустой класс, использующийся для регистрации модуля в спринг-контейнере
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
